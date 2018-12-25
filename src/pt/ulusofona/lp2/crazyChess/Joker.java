286c026ad32183c1a844a2ef1444a80f49df60fd
21703781 <a21703781@alunos.ulht.pt>
2018-12-25 22:57:29 +0000

    a testar

--- src/pt/ulusofona/lp2/crazyChess/Joker.java
+++ src/pt/ulusofona/lp2/crazyChess/Joker.java
@@ -1,11 +1,5 @@
 package pt.ulusofona.lp2.crazyChess;
 
 public class Joker extends CrazyPiece {
-    public String getImagePNG(){
-        if(iDEquipa == 10){
-            return "joker_black.png";
-        }else{
-            return "joker_white.png";
-        }
-    }
+
 }
