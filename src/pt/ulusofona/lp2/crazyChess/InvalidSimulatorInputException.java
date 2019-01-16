84324c6184f646c98ca0c33ce2560000f53c8990
21703781 <a21703781@alunos.ulht.pt>
2019-01-16 09:26:51 +0000

    terceira parte

--- src/pt/ulusofona/lp2/crazyChess/InvalidSimulatorInputException.java
+++ src/pt/ulusofona/lp2/crazyChess/InvalidSimulatorInputException.java
@@ -1,6 +1,6 @@
 package pt.ulusofona.lp2.crazyChess;
 
-public class InvalidSimulatorInputException {
+public class InvalidSimulatorInputException extends Exception{
     int getLinhaErro(){
         return 0;
     }
